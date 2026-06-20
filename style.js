d1821117ef44f1739621edd7ed2bc65d63fd6c35
kirra <up2245047@myport.ac.uk>
2026-06-20 18:03:55 +0100

    added design section + minor fixes

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
-const proj_bttn = document.querySelector("#proj-bttn");
+const proj_bttn = document.querySelector("#proj-bttn"); // get rid of prob
 
 
 const entry = document.querySelector("#proj-sect");
-const landing = document.querySelector("#landing");
+const landing = document.querySelector("#landing"); // get rid of prob
 
 const observer = new IntersectionObserver(
   ([entry]) => {
@@ -22,10 +22,8 @@ observer.observe(entry);
 
 
 const entry2 = document.querySelector("#illustrations");
-const navButtons = document.querySelectorAll('.nav-bttn');
-const headings = document.querySelectorAll('h1');
-
-
+const navButtons = document.querySelectorAll('.nav-bttn'); // get rid of prob
+const headings = document.querySelectorAll('h1'); // get rid of prob
 
 const observer2 = new IntersectionObserver(
   ([entry]) => {
@@ -41,6 +39,24 @@ const observer2 = new IntersectionObserver(
 );
 
 observer2.observe(entry2);
+
+const designSec = document.querySelector("#designs");
+// observer to check if the design page is visible
+// adds proj (change the name) to edit body color to solid blue
+const designSecIsVisible = new IntersectionObserver(
+  ([entry]) => {
+    if (entry.isIntersecting) {
+      document.body.classList.add("proj");
+    }  else {
+      document.body.classList.remove("proj");
+    }
+  },
+  {
+    threshold: 0.7
+  }
+);
+
+observer2.observe(designSec);
 
 
 const preview = document.querySelector('.main-preview');
